e07707f84d5c6be2812e44ce6370da11fa0ee0a4
zzl514 <1146942367@qq.com>
2022-05-18 10:18:10 +0800

    init-project

---
+++ common/src/main/java/com/library/utils/BeanCopyUtils.java
@@ -0,0 +1,36 @@
+package com.library.utils;
+
+
+import org.springframework.beans.BeanUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BeanCopyUtils {
+
+    private BeanCopyUtils() {
+
+    }
+
+    public static <V> V copyBean(Object source, Class<V> clazz) {
+        //创建目标对象
+        V result = null;
+        try {
+            result = clazz.newInstance();
+            //实现属性copy
+            BeanUtils.copyProperties(source, result);
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static <O,V> List<V> copyBeanList(List<O> list, Class<V> clazz) {
+        return list.stream()
+                .map(o -> copyBean(o, clazz))
+                .collect(Collectors.toList());
+    }
+
+
+}
---
+++ library-admin/src/main/java/com/library/admin/LibraryAdminApplication.java
@@ -0,0 +1,16 @@
+package com.library.admin;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan("com.library.mapper")
+@EnableScheduling
+public class LibraryAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAdminApplication.class,args);
+    }
+}
